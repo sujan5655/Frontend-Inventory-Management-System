ad6721894cab600b5e5f497cb0eb209eb31d860a
sujan <chaudharysujan5655@gmail.com>
2026-08-30 14:21:17 +0545

    updated the production url of backend in frontend

--- src/ai/aiApi.ts
+++ src/ai/aiApi.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000/api/ai";
+const API_BASE_URL = "https://api.sujan-chaudhary.com.np/api/products/";
 
 export interface ProductAIResponse {
   question: string;
